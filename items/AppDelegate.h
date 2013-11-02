05a7ecc7a2be22c91ddf562a97d65515205ee5f1
Hiroshi Saito <hiroshi3110@gmail.com>
2013-11-02 22:51:43 +0900

    Initial Commit

---
+++ items/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  items
+//
+//  Created by hiroshi on 2013/11/02.
+//  Copyright (c) 2013年 yakitara.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
